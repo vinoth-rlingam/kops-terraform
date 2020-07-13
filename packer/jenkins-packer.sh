171afb721801ebefa37353e6995671c9ad631560
Vinoth Ramalingam <vinoth.ramalingam@health.nsw.gov.au>
2020-07-13 16:21:02 +1000

    first commit

--- packer/jenkins-packer.sh
+++ packer/jenkins-packer.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 set -ex
 
-cd packer
 ARTIFACT_API=`packer build -machine-readable packer-ami-api.json  |awk -F, '$0 ~/artifact,0,id/ {print $6}'`
 AMI_ID_API=`echo $ARTIFACT_API | cut -d ':' -f2`
 echo 'variable "API_INSTANCE_AMI" { default = "'${AMI_ID_API}'" }' > amivar_API.tf
