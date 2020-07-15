ea6dcd17046848c7321d9ea67da4598e2d1deb89
Vinoth Ramalingam <vinoth.ramalingam@health.nsw.gov.au>
2020-07-15 10:19:39 +1000

    first commit

--- ec2/main.tf
+++ ec2/main.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "kops-bootstrapper" {
   ami                    = "${var.API_INSTANCE_AMI}"
   instance_type          = "t2.micro"
-  vpc_security_group_ids = [aws_security_group.example.id]
+  vpc_security_group_ids = [aws_security_group.ec2-sg.id]
   key_name               = "kubelinuxserver"
 
   # This EC2 Instance has a public IP and will be accessible directly from the public Internet
@@ -9,6 +9,27 @@ resource "aws_instance" "kops-bootstrapper" {
 
   tags = {
     Name = "kops-bootstrapper"
+  }
+}
+
+resource "aws_security_group" "ec2-sg" {
+  name = var.instance_name
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port = 0
+    to_port   = 0
+    protocol  = "tcp"
+
+    # To keep this example simple, we allow incoming SSH requests from any IP. In real-world usage, you should only
+    # allow SSH requests from trusted servers, such as a bastion host or VPN server.
+    cidr_blocks = ["0.0.0.0/0"]
   }
 }
 
