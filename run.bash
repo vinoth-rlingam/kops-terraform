2f412f5f1ef9bfba17f36deb42f9a32ae0bf20bb
Vinoth Ramalingam <vinoth.ramalingam@health.nsw.gov.au>
2020-07-15 13:23:05 +1000

    first commit

--- run.bash
+++ run.bash
@@ -9,10 +9,10 @@ export TF_VAR_stateBucketKey=terraform.tfstate
 
 echo $TF_VAR_stateBucketName "and" $TF_VAR_stateBucketKey "and" $TF_VAR_region
 
-/usr/local/bin/terraform init -input=false -backend-config "bucket=$TF_VAR_stateBucketName" -backend-config "key=$TF_VAR_stateBucketKey" -backend-config "region=$TF_VAR_stateBucketRegion"
+terraform init -input=false -backend-config "bucket=$TF_VAR_stateBucketName" -backend-config "key=$TF_VAR_stateBucketKey" -backend-config "region=$TF_VAR_stateBucketRegion"
 
 echo "Terraform plan...."
-/usr/local/bin/terraform plan -out=tfplan -input=false
+terraform plan -out=tfplan -input=false
 
 echo "Updating Stack...."
-/usr/local/bin/terraform apply -input=false -auto-approve
+terraform apply -input=false -auto-approve
