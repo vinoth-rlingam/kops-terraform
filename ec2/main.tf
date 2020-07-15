df84966006535125d72dbf98bcfbdb5986b40d76
Vinoth Ramalingam <vinoth.ramalingam@health.nsw.gov.au>
2020-07-15 12:22:04 +1000

    first commit

--- ec2/main.tf
+++ ec2/main.tf
@@ -30,9 +30,7 @@ resource "aws_security_group" "ec2-sg" {
     # To keep this example simple, we allow incoming SSH requests from any IP. In real-world usage, you should only
     # allow SSH requests from trusted servers, such as a bastion host or VPN server.
     cidr_blocks = ["0.0.0.0/0"]
-    tags {
-            Name = “Allow SSH”
-        }
+
   }
 }
 
