338634b2bd49cac94359aa46ae030e42a0f94b3b
Vinoth Ramalingam <vinoth.ramalingam@health.nsw.gov.au>
2020-07-15 12:06:42 +1000

    first commit

--- ec2/main.tf
+++ ec2/main.tf
@@ -23,15 +23,6 @@ resource "aws_security_group" "ec2-sg" {
   }
 
   ingress {
-    from_port = 0
-    to_port   = 0
-    protocol  = "tcp"
-
-    # To keep this example simple, we allow incoming SSH requests from any IP. In real-world usage, you should only
-    # allow SSH requests from trusted servers, such as a bastion host or VPN server.
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-  {
     from_port = 22
     to_port   = 22
     protocol  = "ssh"
