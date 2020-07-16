77eff794a3fdded2ae27a249c4a67a536e41a343
Vinoth Ramalingam <vinoth.ramalingam@health.nsw.gov.au>
2020-07-16 13:25:28 +1000

    first commit

--- terraform-kops/kops-kubernetes/create-kops-cluster.bash
+++ terraform-kops/kops-kubernetes/create-kops-cluster.bash
@@ -2,6 +2,9 @@
 set -ex
 
 TF_OUTPUT=$(cd .. && terraform output -json)
-export REGION="$(echo ${TF_OUTPUT} region | jq -r)"
-export KOPS_STATE_STORE="$(echo ${TF_OUTPUT} kops_s3_bucket | jq -r)"
-export NAME="$(echo ${TF_OUTPUT} kubernetes_cluster_name | jq -r)"
+REGION="$(echo ${TF_OUTPUT} region | jq -r)"
+ KOPS_STATE_STORE="$(echo ${TF_OUTPUT} kops_s3_bucket | jq -r)"
+ NAME="$(echo ${TF_OUTPUT} kubernetes_cluster_name | jq -r)"
+export $REGION
+export $KOPS_STATE_STORE
+export $NAME
